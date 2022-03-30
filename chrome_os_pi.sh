dc9cb61645c0ef34dace0d3a129dcfaa505c2770
Joseph <joseph@Josephs-MacBook.local>
2022-03-30 23:51:24 +0900

    chrome os first commit

---
+++ chrome_os_pi.sh
@@ -0,0 +1,25 @@
+
+# install pip3
+sudo apt install python3-pip
+
+
+# install kotlin
+sudo apt install zip
+curl -s "https://get.sdkman.io" | bash
+
+# source current terminal
+
+sdk install kotlin
+
+# install gradle
+
+sdk install gradle
+
+sudo apt update
+
+sudo apt install default-jdk
+
+# vim support for kotlin
+git clone https://github.com/udalov/kotlin-vim.git ~/.vim/pack/plugins/start/kotlin-vim
+
+
